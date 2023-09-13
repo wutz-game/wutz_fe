70b324e94c24d812631a63380ce54bb8bec441d8
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-13 17:53:32 -0400

    Feat: simplifies controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,27 +1,10 @@
 class GamesController < ApplicationController
   def index
-    # Fetch category placeholders from the backend API
-    @categories = fetch_categories_from_backend
-  end
-
-  def show
-    game_url = params[:id]
-    @game_data = fetch_game_data(game_url)
-    @categories = fetch_categories_from_backend
-    @question_placeholder = 'Question Placeholder' # Replace with your desired placeholder text
-    @answer_placeholder = 'Answer Placeholder' # Replace with your desired placeholder text
-    @submit_placeholder = 'Submit Placeholder' # Replace with your desired placeholder text
-  end
-
-  def create
-    redirect_to games_path
-  end
-
-  private
-
-  def fetch_game_data(url); end
-
-  def fetch_categories_from_backend
-    ['Category 1 Placeholder', 'Category 2 Placeholder', 'Category 3 Placeholder']
+    @categories = ['Category 1', 'Category 2', 'Category 3']
+    @questions = [
+      %w[Q1 Q2 Q3],
+      %w[Q4 Q5 Q6],
+      %w[Q7 Q8 Q9]
+    ]
   end
 end
