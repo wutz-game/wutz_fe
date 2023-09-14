76e18ec01e4d1281b7dbbef4aaa9bebafe628f17
Ethan Black <ethanrossblack@gmail.com>
2023-09-14 16:36:30 -0600

    test: formatting for quiz_category spec

--- spec/poros/quiz_category_spec.rb
+++ spec/poros/quiz_category_spec.rb
@@ -1,15 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "Quiz Category PORO" do
+  before do
+    @params = { category: "before & after", category_emoji: "🕰️" }
+
+    @quiz_category = QuizCategory.new(@params)
+  end
+
   it "exists and has attributes" do
-    params = {
-      category: "before & after", 
-      category_emoji: "🕰️" 
-    }
+    expect(@quiz_category.name).to eq(@params[:category])
+    expect(@quiz_category.name).to be_a(String)
 
-    quiz_category = QuizCategory.new(params)
-
-    expect(quiz_category.name).to eq(params[:category])
-    expect(quiz_category.emoji).to eq(params[:emoji])
+    expect(@quiz_category.emoji).to eq(@params[:category_emoji])
+    expect(@quiz_category.emoji).to be_a(String)
   end
 end
