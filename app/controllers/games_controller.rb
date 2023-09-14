3c3a196f1b4981fb1a78b8368a1259822c752d8e
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-13 21:43:20 -0400

    Feat: adds placeholders

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,16 @@
 class GamesController < ApplicationController
   def index
-    @categories = ['Category 1', 'Category 2', 'Category 3']
+    @categories = %w[Geography Science History]
+    @points = [
+      ['1 Point', '1 Point', '1 Point'],
+      ['2 Points', '2 Points', '2 Points'],
+      ['3 Points', '3 Points', '3 Points']
+    ]
     @questions = [
-      %w[Q1 Q2 Q3],
-      %w[Q4 Q5 Q6],
-      %w[Q7 Q8 Q9]
+      ['What is the capital of France?', 'What is the largest continent?', 'What is the longest river?'],
+      ['What is the chemical symbol for water?', 'What planet is known as the Red Planet?',
+       'What is the speed of light?'],
+      ['Who was the first President of the United States?', 'What year did World War II end?', "Who wrote 'Hamlet'?"]
     ]
   end
 end
