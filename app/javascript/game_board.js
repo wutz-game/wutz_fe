3594c14c3f5ebd12ebb4480332a07f06f18e7dd6
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-13 21:43:31 -0400

    Feat: Adds Javascript

--- app/javascript/game_board.js
+++ app/javascript/game_board.js
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const myForm = document.getElementById('myForm');
   const myInput = document.getElementById('myInput');
 
+  // Get the Skip button element
+const skipBtn = document.getElementById('skipBtn');
+
   let lastClickedCell = null; // To keep track of the last clicked cell
 
   // Close button functionality
@@ -17,34 +20,52 @@ document.addEventListener('DOMContentLoaded', function() {
     popup.style.display = 'none';
   });
 
-  // Add click event to each game cell
-  gameCells.forEach(function(cell) {
-    cell.addEventListener('click', function() {
-      const question = cell.getAttribute('data-question');
+// Add click event to each game cell
+gameCells.forEach(function(cell) {
+  cell.addEventListener('click', function() {
+    const question = cell.getAttribute('data-question');
+    const points = cell.textContent;  // Get the points text from the cell
 
-      // Set question text
-      questionElement.textContent = question;
+    // Set question text and points
+    questionElement.textContent = question;
 
-      // Show popup
-      popup.style.display = 'block';
+    // Update the popup header to show the points
+    document.querySelector('#popup h2').textContent = points;
 
-      // Store the last clicked cell
-      lastClickedCell = cell;
-    });
+    // Show popup
+    popup.style.display = 'block';
+
+    // Store the last clicked cell
+    lastClickedCell = cell;
+  });
+});
+
+
+// Add skip button functionality
+skipBtn.addEventListener('click', function() {
+  // Check to make sure a cell was clicked
+  if (lastClickedCell) {
+    lastClickedCell.style.backgroundColor = '#C2BA71';
+  }
+
+    // Reset the form and hide the popup
+    myInput.value = '';
+    result.textContent = '';
+    popup.style.display = 'none';
   });
 
   // Listen for form submission inside popup
   myForm.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    // Just an example, your real answer validation will vary
+    // Just an example, the real answer validation will vary
     const input = myInput.value;
 
     if (lastClickedCell) { // Check to make sure a cell was clicked
       if (input.toLowerCase() === 'yes') {
         lastClickedCell.style.backgroundColor = 'green';
       } else if (input.toLowerCase() === 'maybe') {
-        lastClickedCell.style.backgroundColor = 'yellow';
+        lastClickedCell.style.backgroundColor = '#C2BA71';
       } else {
         lastClickedCell.style.backgroundColor = 'red';
       }
