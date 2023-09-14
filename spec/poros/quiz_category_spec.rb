82601a967830b0b2ed7adebed9e538b91178e61b
Ethan Black <ethanrossblack@gmail.com>
2023-09-13 18:15:46 -0600

    fix: fix quiz_category test syntax

--- spec/poros/quiz_category_spec.rb
+++ spec/poros/quiz_category_spec.rb
@@ -2,11 +2,14 @@ require "rails_helper"
 
 RSpec.describe "Quiz Category PORO" do
   it "exists and has attributes" do
-    params = {category: "before & after", category_emoji: "🕰️" }
+    params = {
+      category: "before & after", 
+      category_emoji: "🕰️" 
+    }
 
     quiz_category = QuizCategory.new(params)
 
-    expect(quiz_category.name).to eq("before & after")
-    expect(quiz_category.emoji).to eq("🕰️")
+    expect(quiz_category.name).to eq(params[:category])
+    expect(quiz_category.emoji).to eq(params[:emoji])
   end
 end
