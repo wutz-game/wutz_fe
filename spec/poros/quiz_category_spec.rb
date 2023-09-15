857e0a62cfd2d4314a9c301b415ff5c067faf6af
Ethan Black <ethanrossblack@gmail.com>
2023-09-14 22:58:48 -0600

    feat: add questions as attributes in quiz_category (and testing)

--- spec/poros/quiz_category_spec.rb
+++ spec/poros/quiz_category_spec.rb
@@ -2,7 +2,15 @@ require "rails_helper"
 
 RSpec.describe "Quiz Category PORO" do
   before do
-    @params = { category: "before & after", category_emoji: "🕰️" }
+    @params = {
+      :category=>"before & after",
+      :category_emoji=>"🕰️",
+      :questions=> [
+        {:answer=>"a putting Green Eggs and Ham", :question=>"Golf short game practice area served up in the title of a Dr. Seuss book", :points=>1},
+        {:answer=>"the Hebrew National League", :question=>"If they used a well-known brand of kosher hot dogs instead of bats, the Dodgers & Phillies would be in this baseball group", :points=>2},
+        {:answer=>"the Cheshire Cat scan", :question=>"Grinning enigmatic acquaintance of Alice in Wonderland seen in cross section by doctors", :points=>3}
+      ]
+    }
 
     @quiz_category = QuizCategory.new(@params)
   end
@@ -13,5 +21,11 @@ RSpec.describe "Quiz Category PORO" do
 
     expect(@quiz_category.emoji).to eq(@params[:category_emoji])
     expect(@quiz_category.emoji).to be_a(String)
+  end
+
+  it "has an array of Question objects" do
+    expect(@quiz_category.questions).to be_an(Array)
+    expect(@quiz_category.questions.length).to eq(3)
+    expect(@quiz_category.questions[0]).to be_a(Question)
   end
 end
