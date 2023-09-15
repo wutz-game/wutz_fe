5ec69bdf1028f0e3fc21343de32b582307e45474
Ethan Black <ethanrossblack@gmail.com>
2023-09-15 12:35:40 -0600

    fix: facade gives questions in nested array

--- app/facades/games_facade.rb
+++ app/facades/games_facade.rb
@@ -1,17 +1,33 @@
 class GamesFacade
 
   def questions
-    @questions = {
-      q1: categories[0].questions[0],
-      q2: categories[1].questions[0],
-      q3: categories[2].questions[0],
-      q4: categories[0].questions[1],
-      q5: categories[1].questions[1],
-      q6: categories[2].questions[1],
-      q7: categories[0].questions[2],
-      q8: categories[1].questions[2],
-      q9: categories[2].questions[2]
-    }
+    @questions = 
+    [
+      [ 
+        categories[0].questions[0],
+        categories[1].questions[0],
+        categories[2].questions[0]
+      ],[ 
+        categories[0].questions[1],
+        categories[1].questions[1],
+        categories[2].questions[1]
+      ],[ 
+        categories[0].questions[2],
+        categories[1].questions[2],
+        categories[2].questions[2]
+      ]
+    ]
+    # {
+    #   q1: categories[0].questions[0],
+    #   q2: categories[1].questions[0],
+    #   q3: categories[2].questions[0],
+    #   q4: categories[0].questions[1],
+    #   q5: categories[1].questions[1],
+    #   q6: categories[2].questions[1],
+    #   q7: categories[0].questions[2],
+    #   q8: categories[1].questions[2],
+    #   q9: categories[2].questions[2]
+    # }
   end
 
   def categories
