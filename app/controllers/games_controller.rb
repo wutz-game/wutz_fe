02809bb0cc7e88c82bd26d77007079a096c3e6d0
Ethan Black <ethanrossblack@gmail.com>
2023-09-14 22:58:57 -0600

    feat: basic games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,6 @@
 class GamesController < ApplicationController
+
   def index
-    @categories = %w[Geography Science History]
-    @points = [
-      ['1 Point', '1 Point', '1 Point'],
-      ['2 Points', '2 Points', '2 Points'],
-      ['3 Points', '3 Points', '3 Points']
-    ]
-    @questions = [
-      ['What is the capital of France?', 'What is the largest continent?', 'What is the longest river?'],
-      ['What is the chemical symbol for water?', 'What planet is known as the Red Planet?',
-       'What is the speed of light?'],
-      ['Who was the first President of the United States?', 'What year did World War II end?', "Who wrote 'Hamlet'?"]
-    ]
-    @correct_answers = [
-      ['Paris', 'Asia', 'Nile'],
-      ['H2O', 'Mars', '299,792,458 m/s'],
-      ['George Washington', '1945', 'William Shakespeare']
-    ]
+    @facade = GamesFacade.new
   end
 end
