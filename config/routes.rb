7c02950b7ecbdbfc199a2f2fcf808cfa5ebb5444
Connor Richmond <richmond.connor@gmail.com>
2023-09-18 18:09:41 -0400

    login and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'games#index'
   resources :games, only: %i[index create]
+
+  resources :users, only: [:new, :create]
+  get "/login", to: "users#login_form"
+  post "/login", to: "users#login"
+  get '/create_account', to: 'users#new', as: 'create_account'
 end
