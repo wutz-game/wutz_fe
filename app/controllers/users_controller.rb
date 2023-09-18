a8a8ffa65f30bbeb6a90058269ec69de684f6fa2
Connor Richmond <richmond.connor@gmail.com>
2023-09-18 18:16:20 -0400

    fix path to root after login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     if user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.username}!"
-      redirect_to game_path
+      redirect_to root_path
     else
       flash[:error] = "Sorry, your credentials are bad."
       render :login_form
