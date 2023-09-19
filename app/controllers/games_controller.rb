4cee0954b54afc688c665b3877d20d6190afbe32
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-18 22:24:21 -0400

    Feat: updats controller to submit results

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,15 @@
 class GamesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:submit_result]  # Add this line
 
   def index
     @facade = GamesFacade.new
+  end
+
+  def submit_result
+    game_result = params[:data]
+    score = game_result['score']
+    user_answers = game_result['user_answers']
+
+    render json: { success: true }
   end
 end
