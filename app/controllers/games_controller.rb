a6a5d8cc421eb39ef7f78de7e1f9029e1577c6cb
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-19 15:26:08 -0400

    Feat: updates controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,44 @@
 class GamesController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:submit_result]  # Add this line
+  skip_before_action :verify_authenticity_token, only: [:submit_result]
 
   def index
     @facade = GamesFacade.new
   end
 
   def submit_result
-    game_result = params[:data]
-    score = game_result['score']
-    user_answers = game_result['user_answers']
+    game_result = game_result_params
+
+    if game_result.nil?
+      render json: { success: false, error: 'No game_result in params' }, status: 400
+      return
+    end
+
+    score = game_result[:score]
+    user_answers = game_result[:user_answers]
+
+    if score.nil? || user_answers.nil?
+      render json: { success: false, error: 'Missing score or user_answers' }, status: 400
+      return
+    end
+
+    # ... add actual logic for handling the game result ...
 
     render json: { success: true }
+  end
+
+  def clear_state
+    # Clear session, cookies, or database state here
+    session.clear
+
+    render json: { success: true, message: 'Server-side game state cleared' }
+  end
+
+  private
+
+  def game_result_params
+    params.require(:data).require(:data).permit(
+      :type, :user_id, :game_id, :score,
+      user_answers: %i[game_question_id user_answer result]
+    )
   end
 end
