7b4a0be30ee6012867a43fbb2c0fcb18597fe9ea
Connor Richmond <richmond.connor@gmail.com>
2023-09-19 22:17:54 -0400

    User also need email when logging in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :email
   validates_presence_of :password
 
   has_secure_password
