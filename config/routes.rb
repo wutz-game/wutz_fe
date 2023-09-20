624f8ffcc43d2d6fbc96d909bec72504a81c6063
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-19 21:31:05 -0400

    Feat: updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'games#index'
+  resources :game_results, only: :index
   resources :games, only: %i[index create]
   post 'games/submit_result', to: 'games#submit_result'
 
