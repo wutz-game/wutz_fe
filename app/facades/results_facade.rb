bbf01b6ff28f7516a63277bd8c67be4544f6f255
Ian Lyell <ian.lyell@yahoo.com>
2023-09-20 11:07:25 -0500

    test: Remerge to fix button

--- app/facades/results_facade.rb
+++ app/facades/results_facade.rb
@@ -1,9 +1,56 @@
 class ResultsFacade
+    attr_reader :score
 
-    def initialize(results)
-        @user = results[:user_id]
-        @score = results[:score]
-        @questions = results[:user_answers]
+    def initialize
+        @score = "-5"
+        @questions = [
+                {
+                  "game_question_id": "cell_0_0",
+                  "user_answer": "test",
+                  "result": "correct"
+                },
+                {
+                  "game_question_id": "cell_0_1",
+                  "user_answer": "test",
+                  "result": "incorrect"
+                },
+                {
+                  "game_question_id": "cell_0_2",
+                  "user_answer": "test",
+                  "result": "correct"
+                },
+                {
+                  "game_question_id": "cell_1_0",
+                  "user_answer": "",
+                  "result": "incorrect"
+                },
+                {
+                  "game_question_id": "cell_1_1",
+                  "user_answer": "low blow down",
+                  "result": "correct"
+                },
+                {
+                  "game_question_id": "cell_1_2",
+                  "user_answer": "low blow down",
+                  "result": "correct"
+                },
+                {
+                  "game_question_id": "cell_2_0",
+                  "user_answer": "low blow down",
+                  "result": "incorrect"
+                },
+                {
+                  "game_question_id": "cell_2_1",
+                  "user_answer": "low blow down",
+                  "result": "incorrect"
+                },
+                {
+                  "game_question_id": "cell_2_2",
+                  "user_answer": "low blow down",
+                  "result": "incorrect"
+                }
+            ]
+
         @cell_0_0 = cell_lookup("cell_0_0")
         @cell_0_1 = cell_lookup("cell_0_1")
         @cell_0_2 = cell_lookup("cell_0_2")
@@ -13,16 +60,27 @@ class ResultsFacade
         @cell_2_0 = cell_lookup("cell_2_0")
         @cell_2_1 = cell_lookup("cell_2_1")
         @cell_2_2 = cell_lookup("cell_2_2")
-end
+    end
 
-    def result_output
-        puts "🐘🚢🚚"
-        puts "#{question_convert(@cell_0_0)}#{question_convert(@cell_0_1)}#{question_convert(@cell_0_2)}"
-        puts "#{question_convert(@cell_1_0)}#{question_convert(@cell_1_1)}#{question_convert(@cell_1_2)}"
-        puts "#{question_convert(@cell_2_0)}#{question_convert(@cell_2_1)}#{question_convert(@cell_2_2)}"
+    def categories
+        "🐘🚢🚚"
+    end
+
+    def results_1
+        
+         "#{question_convert(@cell_0_0)}#{question_convert(@cell_0_1)}#{question_convert(@cell_0_2)}"
+    end
+    
+    def results_2
+        "#{question_convert(@cell_1_0)}#{question_convert(@cell_1_1)}#{question_convert(@cell_1_2)}"
+    end
+
+    def results_3
+        "#{question_convert(@cell_2_0)}#{question_convert(@cell_2_1)}#{question_convert(@cell_2_2)}"
     end
 
     def question_convert(question)
+
         if question[:result] == "correct"
             "🟩"
         elsif question[:result] == "skipped"
