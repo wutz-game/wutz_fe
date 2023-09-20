0450853b59382ea3d7683cd0af1305d7f1e73aad
Connor Richmond <richmond.connor@gmail.com>
2023-09-20 17:55:36 -0400

    mailer config

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module WutzFe
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
