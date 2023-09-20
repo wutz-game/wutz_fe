c82cc05d798d907dd8f53943ae09678f45261b62
Connor Richmond <richmond.connor@gmail.com>
2023-09-20 00:36:27 -0400

    model update for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :email
-  validates_presence_of :password
+  validates :email, presence: true
+  validates :password, presence: true
 
   has_secure_password
 end
