2ffe636c984df5650d203642c71b410e6adb89d9
Connor Richmond <richmond.connor@gmail.com>
2023-09-21 15:59:33 -0400

    setup email to be sent to all users everyday at midnight

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -19,4 +21,7 @@ Rails.application.routes.draw do
   get "/login", to: "users#login_form"
   post "/login", to: "users#login"
   get '/create_account', to: 'users#new', as: 'create_account'
+
+  resources :mailers, only: [:create]
+  mount Sidekiq::Web => '/sidekiq'
 end
