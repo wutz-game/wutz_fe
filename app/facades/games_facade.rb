4bc7daf8cb4645528bfdbd02888f0302a7b42dab
Ian Lyell <ian.lyell@yahoo.com>
2023-09-21 13:55:37 -0500

    feat: Results page displays your results

--- app/facades/games_facade.rb
+++ app/facades/games_facade.rb
@@ -17,17 +17,6 @@ class GamesFacade
         categories[2].questions[2]
       ]
     ]
-    # {
-    #   q1: categories[0].questions[0],
-    #   q2: categories[1].questions[0],
-    #   q3: categories[2].questions[0],
-    #   q4: categories[0].questions[1],
-    #   q5: categories[1].questions[1],
-    #   q6: categories[2].questions[1],
-    #   q7: categories[0].questions[2],
-    #   q8: categories[1].questions[2],
-    #   q9: categories[2].questions[2]
-    # }
   end
 
   def categories
