123821709ef9ff76c7eb4ebfb158cf061b4bb010
Ian Lyell <ian.lyell@yahoo.com>
2023-09-22 14:21:55 -0500

    test: Fix testing for main board

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-
+  config.ignore_localhost = true 
   # Automatically filter sensitive data
   config.filter_sensitive_data('<API_KEY>') { ENV['API_KEY'] }
 
