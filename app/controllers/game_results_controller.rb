7552c5a8429500498efc6f13c8ae2b96bfad83e6
Ian Lyell <ian.lyell@yahoo.com>
2023-09-21 20:27:07 -0500

    feat: POST method finish for save games

--- app/controllers/game_results_controller.rb
+++ app/controllers/game_results_controller.rb
@@ -1,5 +1,16 @@
 class GameResultsController < ApplicationController
   def index 
-    @facade = ResultsFacade.new(cookies[:answeredQuestions], cookies[:gameScore])
+    if session[:user_id] != nil
+      @user = User.find(session[:user_id])
+    end
+    @facade = ResultsFacade.new(cookies[:answeredQuestions], cookies[:gameScore], cookies[:gameCategories])
+  end
+
+  def submit
+    @facade = GamesFacade.new
+    service = GamesService.new
+    serializer = GameSerializer.new(session[:user_id], cookies[:answeredQuestions], cookies[:gameScore], cookies[:gameCategories], @facade.game_data[:data][:id])
+    service.post_game(serializer.serialize)
+    redirect_to root_path
   end
 end
--- app/services/games_service.rb
+++ app/services/games_service.rb
@@ -2,12 +2,21 @@ class GamesService
 
   # Note: This is mocked data, at the moment. This will update when we have an endpoint for the BE. This is the expected return value of a game.
   def conn
-    Faraday.new("https://wutz-be.onrender.com")
+    Faraday.new("https://pacific-wildwood-99462-95c6d81ab3e1.herokuapp.com")
   end
 
   def get_url(url)        
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def post_game(game)
+    response = Faraday.post do |req| 
+      req.url "https://pacific-wildwood-99462-95c6d81ab3e1.herokuapp.com/api/user_games"
+      req.headers[:content_type] = 'application/json'
+      req.body = JSON.generate(user_game: game)
+    end
+    binding.pry
   end
 
   def get_daily_game
